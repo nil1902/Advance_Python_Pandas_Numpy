8c4dea8ec8f2b4252f818fa0478157b3e4a52d94
Nilimesh Pal <nilimeshpal15@gmail.com>
2025-10-16 22:56:23 +0530

    sample data testing

--- pandas_learning/app.py
+++ pandas_learning/app.py
@@ -1,3 +1,6 @@
-import pandas as pd 
-df = pd.read_excel("sample.xlsx")  # remove encoding argument
-print(df.head())
+import pandas as pd
+df=pd.read_csv("sample.csv")    
+print(df)
+print("\n")
+cd=pd.read_excel("sample.xlsx")
+print(cd)
